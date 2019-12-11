ead7cd88069531f2d6100e36f1a0c9b408e6a792
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2019-12-11 21:13:16 +0300

    created api angular library project. #1

---
+++ src/api/src/lib/api.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-api',
+  template: `
+    <p>
+      api works!
+    </p>
+  `,
+  styles: []
+})
+export class ApiComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
