d168f853e0250b8df7d27a323e9765872f91838f
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-13 16:30:24 +0300

    added cemiyet color palette as tailwind extension.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'Aquamarine': '#61efce',
+        'Champagne': '#efdab9',
+        'De York': '#81c08b',
+        'Don Juan': '#584b4f',
+        'Eclipse': '#3d3a3a',
+        'Gulf Stream': '#78b0a0',
+        'Kournikova': '#ffd152',
+        'Orange': '#ebac00',
+        'Schooner': '#8e8373',
+        'Toledo': '#343233',
+        'Turbo': '#ffc31f',
+        'Turquoise': '#45edc6',
+        'White Linen': '#efeae1'
+      }
+    }
   },
   variants: {},
   plugins: []
