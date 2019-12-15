622aaaab560a2918d6a9f567d0b49eddbe6c0514
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2019-12-15 18:09:50 +0300

    fixed production build error by adding @cemiyet prefix to library names.

--- projects/web/src/app/pages/landing-page/landing-page.component.ts
+++ projects/web/src/app/pages/landing-page/landing-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '@cemiyet/api';
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() { }
 
