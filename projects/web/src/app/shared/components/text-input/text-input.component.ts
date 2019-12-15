4ef6c75857cd8c3edbd5fa2af176144a140d2c8b
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2019-12-16 00:03:27 +0300

    added text input component with icon add-on support. #3

---
+++ projects/web/src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+
+import { Color, TextAlignment } from '@cemiyet/core';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent implements OnInit {
+  @Input() id: string;
+  @Input() name: string;
+  @Input() placeholder: string;
+  @Input() bgColor: Color;
+  @Input() textColor: Color;
+  @Input() textAlignment: TextAlignment = 'left';
+
+  @Input() icon: string;
+  @Input() iconColor: Color = 'white-linen';
+  @Input() iconLocation: 'left' | 'right' = 'left';
+
+  constructor() { }
+
+  ngOnInit() { }
+}
