d65d7099c730d1e6091aadbdbfffa977506b47fc
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-17 13:26:23 +0300

    removed components from web shared module.

--- projects/web/src/app/shared/shared.module.ts
+++ projects/web/src/app/shared/shared.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LogoComponent } from './components/logo/logo.component';
-import { ButtonComponent } from './components/button/button.component';
-import { IllustrationComponent } from './components/illustration/illustration.component';
-import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [
-    LogoComponent,
-    ButtonComponent,
-    IllustrationComponent,
-    TextInputComponent
-  ],
-  declarations: [
-    LogoComponent,
-    ButtonComponent,
-    IllustrationComponent,
-    TextInputComponent
-  ],
+  exports: [],
+  declarations: [],
   providers: [],
 })
 export class SharedModule {}
