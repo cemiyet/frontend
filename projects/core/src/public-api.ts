ed7655bdf5fa3b4714af2e3699bae0144b15d468
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-17 14:58:43 +0300

    changed directive prefix naming and file naming of core project.

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of core
  */
 
-export * from './types/FontSize';
-export * from './types/Colors';
-export * from './types/TextAlignment';
+export * from './types/font-size';
+export * from './types/colors';
+export * from './types/text-alignment';
+export * from './types/icon-location';
