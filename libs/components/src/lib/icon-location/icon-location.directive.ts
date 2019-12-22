67c1615c6ef68ce89e2cb00ee681c97974b4f726
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2019-12-22 17:14:37 +0300

    refactored and updated components and core libs.

--- libs/components/src/lib/icon-location/icon-location.directive.ts
+++ libs/components/src/lib/icon-location/icon-location.directive.ts
@@ -11,33 +11,23 @@ export class IconLocationDirective {
   @HostBinding('class.left-0')
   @HostBinding('class.pl-2')
   public get isIconOnLeft(): boolean {
-    return (
-      this.iconLocation === 'left' && this.el.nativeElement.localName === 'i'
-    );
+    return this.iconLocation === 'left' && this.el.nativeElement.localName === 'i';
   }
 
   @HostBinding('class.right-0')
   @HostBinding('class.pr-2')
   public get isIconOnRight(): boolean {
-    return (
-      this.iconLocation === 'right' && this.el.nativeElement.localName === 'i'
-    );
+    return this.iconLocation === 'right' && this.el.nativeElement.localName === 'i';
   }
 
   @HostBinding('class.pl-10')
   public get hasIconOnLeft(): boolean {
-    return (
-      this.iconLocation === 'left' &&
-      this.el.nativeElement.localName === 'input'
-    );
+    return this.iconLocation === 'left' && this.el.nativeElement.localName === 'input';
   }
 
   @HostBinding('class.pr-10')
   public get hasIconOnRight(): boolean {
-    return (
-      this.iconLocation === 'right' &&
-      this.el.nativeElement.localName === 'input'
-    );
+    return this.iconLocation === 'right' && this.el.nativeElement.localName === 'input';
   }
 
   constructor(private el: ElementRef) {}
