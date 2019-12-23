e11b265019c2ef26f6650869cacbc4c4077f56e4
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-12-23 23:38:00 +0300

    added ColorVariantDirective and integrated it to input component #7

--- libs/components/src/lib/elements/elements.module.ts
+++ libs/components/src/lib/elements/elements.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { IconComponent } from './icon/icon.component';
+import { ColorVariantDirective } from './color-variant.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [IconComponent],
+  declarations: [ColorVariantDirective, IconComponent],
   exports: [IconComponent]
 })
 export class ElementsModule {}
