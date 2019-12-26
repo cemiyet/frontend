3ab4190a8da0528c2218dc836e16533e2cdf1e51
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2019-12-26 19:37:14 +0300

    added font-size directive to controlling font size styles.

--- libs/components/src/lib/shared/shared.module.ts
+++ libs/components/src/lib/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ColorVariantDirective } from './color-variant.directive';
+import { FontSizeDirective } from './font-size.directive';
 
 @NgModule({
-  declarations: [ColorVariantDirective],
   imports: [CommonModule],
-  exports: [CommonModule, ColorVariantDirective]
+  declarations: [ColorVariantDirective, FontSizeDirective],
+  exports: [CommonModule, ColorVariantDirective, FontSizeDirective]
 })
 export class SharedModule {}
