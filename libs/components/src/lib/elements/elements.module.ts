4398505cb8ec3292802c1e6b21e752987bd7e8e5
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2020-01-02 22:58:29 +0300

    added basic tag component. #7

--- libs/components/src/lib/elements/elements.module.ts
+++ libs/components/src/lib/elements/elements.module.ts
@@ -6,10 +6,11 @@ import { LogoComponent } from './logo/logo.component';
 import { TextComponent } from './text/text.component';
 import { IconComponent } from './icon/icon.component';
 import { ButtonComponent } from './button/button.component';
+import { TagComponent } from './tag/tag.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [LogoComponent, TextComponent, IconComponent, ButtonComponent],
-  exports: [LogoComponent, TextComponent, IconComponent, ButtonComponent]
+  declarations: [LogoComponent, TextComponent, IconComponent, ButtonComponent, TagComponent],
+  exports: [LogoComponent, TextComponent, IconComponent, ButtonComponent, TagComponent]
 })
 export class ElementsModule {}
