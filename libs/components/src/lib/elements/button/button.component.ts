3c7ff6c6a0ed06896c9ff1fa30f96438b7fa0f20
Nuri Uzunoğlu <nuriu@protonmail.com>
2020-01-02 22:58:46 +0300

    removed elementref from button component construction. #7

--- libs/components/src/lib/elements/button/button.component.ts
+++ libs/components/src/lib/elements/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ColorVariant, FontSize } from '@cemiyet/core';
 
 @Component({
@@ -11,7 +11,7 @@ export class ButtonComponent implements OnInit {
   @Input() variant: ColorVariant = 'LIGHT';
   @Input() outline: boolean;
 
-  constructor(private el: ElementRef) {}
+  constructor() {}
 
   ngOnInit() {
     this.outline = this.outline !== undefined;
