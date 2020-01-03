a5bc26e0eed3d59f62838f065c2433e5951fbf35
Nuri Uzunoğlu <nuriu@protonmail.com>
2020-01-03 22:56:04 +0300

    added removable option to tag component. #7

--- libs/components/src/lib/elements/tag/tag.component.ts
+++ libs/components/src/lib/elements/tag/tag.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ColorVariant, FontSize } from '@cemiyet/core';
 
 @Component({
@@ -9,11 +9,35 @@ import { ColorVariant, FontSize } from '@cemiyet/core';
 export class TagComponent implements OnInit {
   @Input() size: FontSize = 'xs';
   @Input() variant: ColorVariant = 'LIGHT';
+  @Input() removable: boolean;
   @Input() outline: boolean;
 
-  constructor() {}
+  private iconVariant: ColorVariant = 'LIGHT';
+  private removed = false;
+
+  constructor(private el: ElementRef) {}
 
   ngOnInit() {
+    this.removable = this.removable !== undefined;
     this.outline = this.outline !== undefined;
+
+    switch (this.variant) {
+      case 'LIGHT':
+      case 'PRIMARY':
+      case 'SECONDARY':
+        this.iconVariant = 'DARK';
+        break;
+    }
+
+    if (this.outline) {
+      this.iconVariant = this.variant;
+    }
+  }
+
+  onRemoveClicked(event: Event) {
+    event.stopImmediatePropagation();
+
+    const element = this.el.nativeElement as HTMLElement;
+    element.parentNode.removeChild(element);
   }
 }
