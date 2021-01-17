73467d07064d7c2111c651aa5d953da8c830cf3c
Nuri Uzunoğlu <nuriu@protonmail.com>
2021-01-17 13:24:25 +0300

    removed old structure.

--- libs/core/README.md
+++
@@ -1,3 +0,0 @@
-# Core
-
-> Provides core types.
--- libs/core/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types/color.type';
-export * from './types/font.type';
-export * from './types/icon.type';
-export * from './types/text.type';
